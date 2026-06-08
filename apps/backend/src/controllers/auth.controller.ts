ec9f4daf8daa404a2ce6fca8d864d6528822a08c
Himanshu <ht5620085@gmail.com>
2026-06-08 19:57:15 +0530

    adding the basic controller for auth register, sign in, verify, me, and logout

--- apps/backend/src/controllers/auth.controller.ts
+++ apps/backend/src/controllers/auth.controller.ts
@@ -8,22 +8,64 @@ import { type AuthRequest } from '../middleware/middleware';
 import { generateToken } from '../utils/generateToken';
 
 
+export const verify = async (req: AuthRequest, res: Response) => {
+    try {
+        const token = req.query.token as string;
+
+        if (!token) {
+            return res.status(400).json({ message: "token not found" })
+        }
+
+        const [user] = await db.select().from(users).where(eq(users.verificationToken, token))
+
+        if (!user) {
+            return res.status(400).json({ message: "invalid token" })
+        }
+
+        if (new Date() > user.verificationTokenExpiry!) {
+            return res.status(400).json({ message: "token expired" })
+        }
+
+        const [updatedUser] = await db.update(users).set({
+            isVerified: true,
+            verificationToken: null,
+            verificationTokenExpiry: null,
+        }).where(eq(users.id, user.id)).returning();
+
+        // generate jwt token
+        const jwtToken = generateToken(user.id)
+
+        // set cookie
+
+        // return success message
+        return res.status(200).json({ message: "success" })
+    } catch (error) {
+        return res.status(500).json({ message: "internal server error" })
+    }
+}
+
+
 export const register = async (req: AuthRequest, res: Response) => {
     const { username, email, password } = req.body;
 
     try {
         const existingUser = await db.select().from(users).where(eq(users.email, email));
 
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists. Please sign-in.' });
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: 'Invalid email or password.' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
+        const verification_token = crypto.randomUUID();
+
         const [newUser] = await db.insert(users).values({
             name: username,
             email,
-            password: hashedPassword
+            password: hashedPassword,
+            isVerified: false,
+            verificationToken: verification_token,
+            verificationTokenExpiry: new Date(Date.now() + 24 * 60 * 60 * 1000)
         }).returning();
 
         res.status(201).json({ message: "check your email" });
@@ -37,43 +79,45 @@ export const login = async (req: AuthRequest, res: Response) => {
     const { email, password } = req.body;
 
     try {
-        const user = await db.select().from(users).where(eq(users.email, email)).then(result => result[0]);
+        const [user] = await db.select().from(users).where(eq(users.email, email));
 
         if (!user) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
 
         const isMatch = await bcrypt.compare(password, user.password as string);
+        const isVerified = user.isVerified;
 
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
 
-        const token = generateToken(user.id);
-
-        res.json({ token });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
-    }
-};
-
-export const getProfile = async (req: AuthRequest, res: Response) => {
-    try {
-        const user = await db.select().from(users).where(eq(users.id, req.user?.id as string)).then(result => result[0]);
-
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+        if (!isVerified) {
+            return res.status(400).json({ message: "User is not verified. Please verify your email." })
         }
 
-        res.json({ user });
+        const token = generateToken(user.id);
+
+        // set the auth cookie as the jwt token generated
+
+        // success message
+        res.json({ message: "logged in" });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server error' });
     }
 };
 
-export const logout = (req: AuthRequest, res: Response) => {
-    // Invalidate the token on the client side by clearing it
-    res.json({ message: 'Logged out successfully' });
-};
+export const me = async (req: AuthRequest, res: Response) => {
+    if (!req.user) {
+        return res.status(400).json({ message: "unauthorized" })
+    }
+
+    res.json({ id: req.user.id })
+}
+
+export const logout = async (req: AuthRequest, res: Response) => {
+    // clear the auth cookie
+
+    res.json({ message: "Logged out" })
+}
